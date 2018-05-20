4fe69471b9adcd31d697c2cf83bf7f5d176b160e
ruusty <russell@holliday.id.au>
2018-05-20 14:16:23 +1000

    Got asynchronicity working sort of

---
+++ Specification/Start-ExeWithOutput.Module.Tests.ps1
@@ -0,0 +1,115 @@
+﻿[CmdletBinding()]
+param
+(
+)
+#region initialisation
+if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent)
+{
+  $VerbosePreference = 'Continue'
+}
+$Verbose = @{ }
+if ($env:COMPUTERNAME -like "flores")
+{
+  $Verbose.add("Verbose", $false)
+}
+
+<#
+#invoke-Pester -Script @{ Path = './start-ExeWithOutput.Module.Tests.ps1'; verbose = 'Continue' }
+#
+invoke-Pester -Script @{ Path = './start-ExeWithOutput.Module.Tests.ps1'; }
+##>
+Write-Host $('{0}==>{1}' -f '$Verbose', $Verbose)
+Write-Host $('{0}==>{1}' -f '$VerbosePreference', $VerbosePreference)
+$PSBoundParameters | Out-String | Write-Verbose
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+#$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+$name = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Path)
+$ModuleName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Module.Tests.ps1"
+$name = $ModuleName
+Write-Verbose $('{0}:{1}' -f '$here', $here)
+Write-Verbose $('{0}:{1}' -f '$sut', $sut)
+Write-Verbose $('{0}:{1}' -f '$ModuleName', $ModuleName)
+
+$ModulePath = Join-Path $here "..\StartExeWithOutput\StartExeWithOutput\bin\Release\StartExeWithOutput.dll"
+
+
+$ModuleSetup= Join-Path $PSScriptRoot "Pester.Tests.Setup.ps1"
+if (Test-Path $ModuleSetup) {. $ModuleSetup }
+
+
+Import-Module $ModulePath -verbose
+get-module startExeWithOutput
+get-module startExeWithOutput | select -expand ExportedCommands
+$(get-module startExeWithOutput).ExportedCommands.Keys | % { get-help $_ }
+
+
+
+Describe "Start-ExeWithOutput" {
+  Context 'Parameters' {
+    It "Should support -whatif "{
+      (!(((Get-Command -Name Start-ExeWithOutput).parameters["Whatif"]) -eq $null)) | Should be $true
+    }
+  }
+  
+  Context -Name 'Input'{
+    It "Should fail for non-Existant Exe" -Pending {
+      { Start-ExeWithOutput "not-found.exe" } | Should  throw
+    }
+
+  It "Should fail with no args for Ping.exe" {
+      { Start-ExeWithOutput "ping.exe" } | Should  throw
+    }
+
+    It "Should succeed with single arg for Ping.exe" {
+      {Start-ExeWithOutput "ping.exe" "8.8.8.8" @Verbose} | Should not throw
+    }
+
+    It "Should succeed with array of args for Ping.exe" {
+      {Start-ExeWithOutput -filepath "ping.exe" @("8.8.8.8", "-n", "5") @Verbose} | Should not throw
+    }
+    It "Should Throw with array for ver "{
+      { Start-ExeWithOutput -filepath "cmd.exe" @("/c", "ver", "/invalid") @Verbose } | Should throw
+    }
+  }
+
+  Context 'Execution' {
+  It "Should not fail with array of args" {
+    {
+      Start-ExeWithOutput -filepath "whoami.exe" -args @("/ALL","/FO","LIST") @Verbose
+    } | Should not throw
+  }
+
+    It "should throw on non 0 exit " {
+      {
+        Start-ExeWithOutput "whoami.exe","/invalid-arg"
+      }| Should throw
+    }
+
+    It "Should not fail with array of args" {
+    {
+      Start-ExeWithOutput "nslookup.exe" @("-querytype=mx", "holliday.id.au") @Verbose
+      } | Should not throw
+    }
+
+    It "Should not fail with array of args" {
+      {Start-ExeWithOutput "nslookup.exe" @("-querytype=ns", "holliday.id.au") @Verbose} | Should not throw
+    }
+
+  }
+
+  Context 'Output'  {
+
+  It "Should Contain arguments in output" {
+    Start-ExeWithOutput -filepath "echo-args.exe" @("arg1","arg2","arg3") | Should contain '1: arg1'
+  }
+
+    It "Should Contain arguments in output #2" {
+      Start-ExeWithOutput -filepath "echo-args.exe" @("arg1", "arg2", "arg3")     | Should contain '1: arg1'
+    }
+
+    It "Should success with array for dir"{
+      { Start-ExeWithOutput -filepath "cmd.exe" @("/c", "dir", "/w") @Verbose } | Should not throw
+    }
+  }
+}
--- StartExeWithOutput/StartExeWithOutput/StartExeWithOutput.cs
+++ StartExeWithOutput/StartExeWithOutput/StartExeWithOutput.cs
@@ -1,32 +1,50 @@
 ﻿using System;
 using System.ComponentModel;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management.Automation;
 using System.Threading;
-using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections;
 
 namespace RuustyPowerShellModules
 {
+    /// <summary>
+    /// <paratype="synopsis >Runs an executable</paratype>
+    /// <para type="description">Runs an executable and displays the stdout and stderr to Write-Verbose</para>
+    /// <para type="description">Can also write stdout and stderr to log files</para>
+    /// </summary>
+    /// <example>
+    ///   <code>Start-ExeWithOutput -FilePath "sleep.exe" -ArgumentList 5</code>
+    ///   <para>Sleep for 5 seconds</para>
+    /// </example>
+    /// <code>Start-ExeWithOutput -FilePath "ping.exe" -ArgumentList @("127.0.0.1", "-n", "5") -verbose </code>
+    ///  <para>Ping localhost 5 times and display output on Write-Verbose</para>
+    /// <example>
+    /// 
+    /// </example>
     [Cmdlet(VerbsLifecycle.Start, "ExeWithOutput")]
     public class StartExeWithOutput : Cmdlet
     {
-        private bool _abortWrite;
-        private ProgressRecord _progressRecord;
-
         private AsyncOperation _asyncOp;
         private AutoResetEvent _autoResetEvent;
 
+        Nullable<int> _processId = null;
+        private Task<int> task = null;
+        
         private string[] argCollection;
+        string args = string.Empty;
+        private int[] exitCodeCollection =  {0};
+        private List<int> exitCodeList;
 
-        [Parameter(Position = 0, Mandatory = true, HelpMessage = "Executable to start")]
+       [Parameter(Position = 0, Mandatory = true, HelpMessage = "Executable to start")]
         public string FilePath { get; set; }
 
-        [Parameter(Position = 1, HelpMessage = "Arguments for executable")]
+        [Parameter(Position = 1, HelpMessage = "Arguments for FilePath executable")]
         public string[] ArgumentList
         {
             get { return argCollection; }
@@ -34,142 +52,123 @@ namespace RuustyPowerShellModules
         }
 
         [Parameter(Mandatory = false, HelpMessage = "Working Directory")]
-        public string cwd { get; set; } = "";
+        public string WorkingDirectory { get; set; } = Directory.GetCurrentDirectory();
 
+
+        /// <summary>
+        /// <para type="description">Specifies the optional path and file name where the stdout are written.</para>
+        /// </summary>
+        [Parameter( Mandatory = false,
+            HelpMessage = "Enter Stdout log path")]
+        public string LogPathStdout { get; set; }
+
+        /// <summary>
+        /// <para type="description">Specifies the optional path and file name where the stderr are written.</para>
+        /// </summary>
+        [Parameter(Mandatory = false,
+            HelpMessage = "Enter Stderr log path")]
+        public string LogPathStderr { get; set; }
+        [Parameter (Mandatory = false,HelpMessage ="Valid Exit codes")]
+        public int[] ExitCodeList
+        {
+            get { return exitCodeCollection; }
+            set { exitCodeCollection = value; }
+        }
 
         protected override void BeginProcessing()
         {
-            SynchronizationContext.SetSynchronizationContext(new WindowsFormsSynchronizationContext());
+            base.BeginProcessing(); SynchronizationContext.SetSynchronizationContext(new WindowsFormsSynchronizationContext());
             _asyncOp = AsyncOperationManager.CreateOperation(null);
             _autoResetEvent = new AutoResetEvent(false);
+
+            if (argCollection != null)
+            {
+                args = String.Join(" ", argCollection);
+            }
+            exitCodeList = ExitCodeList.ToList();
         }
 
+#pragma warning disable 1591
         protected override void ProcessRecord()
-        {
-            var task = Task.Factory.StartNew(LaunchProcess);
-            do
-            {
-                Application.DoEvents();
-            }
-            while (!_autoResetEvent.WaitOne(250));
-            Application.DoEvents();
-            task.Wait();
-        }
-
-        protected override void StopProcessing()
-        {
-            WriteDebug(string.Format("StopProcessing ThreadId: {0} - {1}", Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.Name));
-            _abortWrite = true;
-            WriteWarning("Aborting ....");
-        }
-
-        protected override void EndProcessing()
-        {
-            Debug.WriteLine("EndProcessing ThreadId: " + Thread.CurrentThread.ManagedThreadId);
-        }
-
-        void DoLongOperation()
-        {
-            string args = string.Empty;
-            if (argCollection != null)
-            {
-                args = String.Join(" ", argCollection);
-            }
-            _progressRecord = new ProgressRecord(0, string.Format("Starting - {0}", FilePath), args);
-
-            Debug.WriteLine("DoWork ThreadId: " + Thread.CurrentThread.ManagedThreadId);
-                for (int i = 0; i < 10; i++)
-                {
-                    _asyncOp.Post(WriteProcessAsync, string.Format("message:{0}",i));
-                    Thread.Sleep(500);
-                }
-
-
-            _asyncOp.Post(WriteWarningAsync,"Just about to exit");
-           _autoResetEvent.Set();
-        }
-
-        void LaunchProcess()
-        {
-            string args = string.Empty;
-            if (argCollection != null)
-            {
-                args = String.Join(" ", argCollection);
-            }
-            _progressRecord = new ProgressRecord(0, string.Format("Starting - {0}", FilePath), args);
-            //WriteProgress(_progressRecord);
-            //Debug.WriteLine("DoWork ThreadId: " + Thread.CurrentThread.ManagedThreadId);
-            for (int i = 0; i < 10; i++)
-            {
-                _asyncOp.Post(WriteProcessAsync, string.Format("message:{0}", i));
-                Thread.Sleep(500);
-            }
-            _asyncOp.Post(WriteErrorAsync, "My Error Message2");
-            ////* Create your Process
-            Process process = new Process();
-            process.StartInfo.FileName = FilePath;
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardError = false;// true;
-
-            //* Optional process configuration
-            if (!String.IsNullOrEmpty(args)) { process.StartInfo.Arguments = args; }
-            if (!String.IsNullOrEmpty(cwd)) { process.StartInfo.WorkingDirectory = cwd; }
-
-            process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
-            {
-                //Console.WriteLine(e.Data);
-                //this.WriteVerbose(e.Data);
-                _asyncOp.Post(WriteProcessAsync, e.Data);
-
-            });
-
-            //throw new InvalidDataException("Unable to read WIM image name");
-            //* Start process and handlers
+        {//process each item in the pipeline
+            _asyncOp.Post(WriteProgressAsync, args);
             try
             {
-                process.Start();
-               // process.BeginOutputReadLine();
-                //process.BeginErrorReadLine();
-                process.WaitForExit();
-                _asyncOp.Post(WriteProcessAsync,"After WaitForExit");
-                _asyncOp.Post(WriteErrorAsync,"My Error Message");
+                task = Task<int>.Factory.StartNew(() =>
+                {
+                    return Start(FilePath, args, WorkingDirectory, LogPathStdout, LogPathStderr);
+                });
+                do
+                {
+                    Debug.WriteLine("ProcessRecord-Message Pump Loop");
+                    Application.DoEvents();
+                }
+                while (!_autoResetEvent.WaitOne(250));
+                Application.DoEvents();
+                task.Wait();
 
-                //throw new System.Management.Automation.RuntimeException(string.Format("{0} ExitCode:{1}", FilePath, process.ExitCode));
-
-    }
-            catch (Exception e)
+                Trace.WriteLine(string.Format("Task.Result='{0}'", task.Result));
+                if (!exitCodeList.Contains( task.Result))
+                {
+                    string ExitCodeCsv = string.Empty;
+                    ExitCodeCsv = string.Join(",", exitCodeList.Select(p => p));
+                    var e = new System.Management.Automation.RuntimeException(String.Format("{0} ExitCode '{1}' not in valid exit codes ({2})", FilePath, task.Result, ExitCodeCsv));
+                    var errorRecord = new ErrorRecord(e, string.Format("Unexpected ExitCode of {0} for {1}", task.Result, FilePath), ErrorCategory.InvalidResult, null);
+                    WriteError(errorRecord);
+                }
+            }
+            catch (AggregateException ae)
             {
-                _asyncOp.Post(WriteWarningAsync,e.StackTrace);
-                ErrorRecord er = new ErrorRecord(e, "", ErrorCategory.InvalidOperation, FilePath);
-                //base.ThrowTerminatingError(er);
-                //throw new System.Management.Automation.RuntimeException(string.Format("{0} ExitCode:{1}", FilePath, process.ExitCode), e);
-                //throw new InvalidDataException("Unable to read WIM image name");
+                var errorRecord = new ErrorRecord(ae.InnerException, "Unexpected ExitCode for " + ae.InnerException.Message, ErrorCategory.InvalidResult, null);
+                WriteError(errorRecord);
+                Console.WriteLine("Task has " + task.Status.ToString());
+                Console.WriteLine(ae.InnerException);
             }
             finally
             {
-                if (process.ExitCode != 0)
-                {
-                    throw new System.Management.Automation.RuntimeException(string.Format("{0} ExitCode:{1}", FilePath, process.ExitCode));
-                    //ErrorRecord er = new ErrorRecord(e, "", ErrorCategory.InvalidOperation, FilePath);
-                    //base.ThrowTerminatingError(er);
-
-                }
+                task.Dispose();
             }
-            _asyncOp.Post(WriteWarningAsync, "Just about to exit");
+            WriteObject(task.Result);
+        }
+
+        protected override void StopProcessing()
+        {//to handle abnormal termination
+            WriteDebug(string.Format("StopProcessing ThreadId: {0} - {1}", Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.Name));
+            WriteWarning("Aborting ....");
             _autoResetEvent.Set();
         }
 
-        private void WriteProcessAsync(object message)
-        {
-            //_progressRecord.StatusDescription = "((WimMessageProcess)message).Path";
-            //_progressRecord.StatusDescription = (string)message;
-            //WriteProgress(_progressRecord);
-            //WriteVerbose("WriteProcessAsync");
-            WriteObject((string)message);
+        protected override void EndProcessing()
+        {//do the finalization
+            Debug.WriteLine("EndProcessing ThreadId: " + Thread.CurrentThread.ManagedThreadId);
         }
-  
+
+        /// 
+        ///  _asyncOp.Post(WriteProcessAsync,string)
+        /// 
+        /// 
+        private void WriteProgressAsync(object message)
+        {
+            string msg = (string)message;
+            ProgressRecord _progressRecord;
+            _progressRecord = new ProgressRecord(0, FilePath, msg);
+            if (String.IsNullOrEmpty(msg))
+            {
+                msg = " ";
+            }
+            WriteProgress(_progressRecord);
+        }
+
+
+        private void WriteObjectAsync(object returnCode)
+        {
+            WriteObject(returnCode);
+        }
+
+        private void WriteVerboseAsync(object message)
+        {
+            WriteVerbose((string)message);
+        }
 
         private void WriteWarningAsync(object message)
         {
@@ -179,11 +178,97 @@ namespace RuustyPowerShellModules
 
         private void WriteErrorAsync(object message)
         {
-            var errorMessage = "Testing exceptions";
-            var errorRecord = new ErrorRecord(new Exception(), errorMessage,    ErrorCategory.WriteError, null);
+            var errorMessage = (string)message;
+            var errorRecord = new ErrorRecord(new System.Management.Automation.RuntimeException(errorMessage), errorMessage,    ErrorCategory.WriteError, null);
             WriteError(errorRecord);
         }
 
+        //
+        // Do the needful
+        private int Start(
+            string filePath,
+            string args = "",
+            string cwd = "",
+            string StdoutLogPath = "",
+            string StderrLogPath = ""
+            )
+    {
+        object _locker = new object();
+        Trace.WriteLine(string.Format("Starting '{0}' {1}", filePath, args));
 
+        //* Create your Process
+        Process process = new Process();
+        process.StartInfo.FileName = filePath;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.RedirectStandardOutput = false;
+        process.StartInfo.RedirectStandardError = false;
+
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+
+        bool isStdoutLogFileRedirect = (!String.IsNullOrEmpty(StdoutLogPath));
+        bool isStderrLogFileRedirect = (!String.IsNullOrEmpty(StderrLogPath));
+        //* Set output and error (asynchronous) handlers
+            process.OutputDataReceived += (s, e) =>
+        {
+            if (isStdoutLogFileRedirect)
+            {
+                lock (_locker)
+                {
+                    File.AppendAllLines(StdoutLogPath, new string[] { e.Data });
+                }
+            }
+            _asyncOp.Post(WriteVerboseAsync, (string)e.Data);
+            Console.WriteLine(e.Data);
+        };
+
+        process.ErrorDataReceived += (s, e) =>
+        {
+            if (isStderrLogFileRedirect)
+            {
+                lock (_locker)
+                {
+                    File.AppendAllLines(StderrLogPath, new string[] { e.Data });
+                }
+            }
+            if (!String.IsNullOrEmpty(e.Data))
+            {
+            _asyncOp.Post(WriteVerboseAsync, (string)e.Data);
+            Console.WriteLine("STDERR>" + e.Data + "STDERR");
+            }
+        };
+
+        process.Exited += (s, e) =>
+        {
+            string msg = string.Format("Exit time:{0} Exit code:{1}", process.ExitTime, process.ExitCode);
+            Console.WriteLine(msg);
+            _asyncOp.Post(WriteVerboseAsync, msg);
+            _autoResetEvent.Set();
+        };
+
+
+        //* Optional process configuration
+        if (!String.IsNullOrEmpty(args)) { process.StartInfo.Arguments = args; }
+        if (!String.IsNullOrEmpty(cwd)) { process.StartInfo.WorkingDirectory = cwd; }
+
+        //* Start process and handlers
+        try
+        {
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            _processId = process.Id;
+            process.WaitForExit();
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex.Message);
+            var e = new System.Management.Automation.RuntimeException(String.Format("{0} ExitCode:{1}", FilePath, -1), ex);
+            _autoResetEvent.Set();
+            throw (e);
+        }
+        return process.ExitCode;
     }
+  }
 }
